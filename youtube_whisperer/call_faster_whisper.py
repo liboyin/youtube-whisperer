800c65cb5967690661933b7d71cd110c47353b3b
Libo Yin <liboyin830@gmail.com>
2024-03-22 14:30:10 +0000

    Simplified model parameter logic

--- youtube_whisperer/call_faster_whisper.py
+++ youtube_whisperer/call_faster_whisper.py
@@ -1,45 +1,32 @@
+import multiprocessing
 import os
 from pathlib import Path
 from typing import Any
 
+from ctranslate2 import get_cuda_device_count
 from faster_whisper import WhisperModel
 import numpy as np
-import torch
 
-
-def get_model(model_name: str, model_path: str, device: str, quantization: str) -> WhisperModel:
-    return WhisperModel(
-        model_size_or_path=model_name,
-        download_root=model_path,
-        device=device,
-        compute_type=quantization,
-    )
-
-
-def get_model_parameters_from_env() -> dict[str, str]:
-    device = os.getenv("ASR_DEVICE", "cuda" if torch.cuda.is_available() else "cpu")
-    quantization = os.getenv("ASR_QUANTIZATION", None)
-    if not quantization:
-        # More about available quantization levels is here: https://opennmt.net/CTranslate2/quantization.html
-        if device == "cuda":
-            quantization = "float32"
-        elif device == "cpu":
-            quantization = "int8"
-        else:
-            raise ValueError(f"Unknown device: {device}")
-    return {
-        "model_name": os.getenv("ASR_MODEL", "large-v3"),
-        "model_path": os.getenv("ASR_MODEL_PATH", str(Path.home() / ".cache" / "whisper")),
-        "device": device,
-        "quantization": quantization,
+def get_default_whisper_model_parameters() -> dict[str, Any]:
+    result = {
+        "model_size_or_path": os.getenv("ASR_MODEL", "large-v3"),
+        "download_root": os.getenv("ASR_MODEL_PATH", str(Path.home() / ".cache" / "whisper")),
     }
-
+    result["device"] = device = "cuda" if get_cuda_device_count() else "cpu"
+    # More about available quantization levels is here: https://opennmt.net/CTranslate2/quantization.html
+    if device == "cuda":
+        result["compute_type"] = "float32"
+    elif device == "cpu":
+        result["compute_type"] = "int8"
+        result["cpu_threads"] = multiprocessing.cpu_count()
+    else:
+        raise ValueError(f"Unknown device: {device}")
+    return result
 
 def transcribe(model: WhisperModel, waveform: np.ndarray, language: str) -> dict[str, Any]:
     segment_generator, _ = model.transcribe(waveform, beam_size=5, task='transcribe', language=language)
     return {"segments": list(segment_generator)}
 
-
-def transcribe_with_model_from_env(waveform: np.ndarray, language: str) -> dict[str, Any]:
-    model = get_model(**get_model_parameters_from_env())
+def transcribe_with_default_model(waveform: np.ndarray, language: str) -> dict[str, Any]:
+    model = WhisperModel(**get_default_whisper_model_parameters())
     return transcribe(model, waveform, language)
