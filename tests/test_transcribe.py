021bf6d55f9a389bdecbddce179d89f737b9c2c8
Libo Yin <liboyin830@gmail.com>
2024-03-29 01:27:28 +0000

    Cleaned up auto-generated tests

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 from unittest import mock
 
@@ -42,29 +43,57 @@ def test_get_default_cuda_flag_without_env_var_cpu(mock_get_cuda_device_count, m
 
 
 @mock.patch('ctranslate2.get_cuda_device_count', return_value=1)
-def test_get_default_cuda_flag_without_env_var_gpu(mock_get_cuda_device_count, monkeypatch):
+def test_get_default_cuda_flag_without_env_var_cuda(mock_get_cuda_device_count, monkeypatch):
     monkeypatch.delenv("ASR_USE_CUDA", raising=False)
     assert get_default_cuda_flag() == True
     mock_get_cuda_device_count.assert_called_once()
 
 
-def test_get_default_whisper_model_parameters_cpu(mocker):
-    mocker.patch('os.getenv', return_value='test')
-    mocker.patch('ctranslate2.get_cuda_device_count', return_value=0)
+def test_get_default_whisper_model_parameters_with_env_var_cpu(monkeypatch, mocker):
+    monkeypatch.setenv("ASR_MODEL", "small")
+    monkeypatch.setenv("ASR_MODEL_PATH", "test/path")
+    monkeypatch.setenv("ASR_USE_CUDA", "false")
     mocker.patch('multiprocessing.cpu_count', return_value=8)
-    result = get_default_whisper_model_parameters()
-    assert result['model_size_or_path'] == 'test'
-    assert result['download_root'] == 'test'
-    assert result['device'] == 'cpu'
-    assert result['compute_type'] == 'int8'
-    assert result["cpu_threads"] == 8
+    assert get_default_whisper_model_parameters() == {
+        "model_size_or_path": "small",
+        "download_root": "test/path",
+        "device": "cpu",
+        "compute_type": "int8",
+        "cpu_threads": 8,
+    }
 
 
-def test_get_default_whisper_model_parameters_gpu(mocker):
-    mocker.patch('os.getenv', return_value='test')
-    mocker.patch('ctranslate2.get_cuda_device_count', return_value=1)
-    result = get_default_whisper_model_parameters()
-    assert result['model_size_or_path'] == 'test'
-    assert result['download_root'] == 'test'
-    assert result['device'] == 'cuda'
-    assert result['compute_type'] == 'float32'
+def test_get_default_whisper_model_parameters_with_env_var_cuda(monkeypatch):
+    monkeypatch.setenv("ASR_MODEL", "small")
+    monkeypatch.setenv("ASR_MODEL_PATH", "test/path")
+    monkeypatch.setenv("ASR_USE_CUDA", "true")
+    assert get_default_whisper_model_parameters() == {
+        "model_size_or_path": "small",
+        "download_root": "test/path",
+        "device": "cuda",
+        "compute_type": "float32",
+    }
+
+
+def test_get_default_whisper_model_parameters_without_env_var_cpu(mocker):
+    mocker.patch('youtube_whisperer.transcribe.get_default_cuda_flag', return_value=False)
+    mocker.patch('pathlib.Path.home', return_value=Path("/test"))
+    mocker.patch('multiprocessing.cpu_count', return_value=8)
+    assert get_default_whisper_model_parameters() == {
+        "model_size_or_path": "large-v3",
+        "download_root": "/test/.whisper",
+        "device": "cpu",
+        "compute_type": "int8",
+        "cpu_threads": 8,
+    }
+
+
+def test_get_default_whisper_model_parameters_without_env_var_cuda(mocker):
+    mocker.patch('youtube_whisperer.transcribe.get_default_cuda_flag', return_value=True)
+    mocker.patch('pathlib.Path.home', return_value=Path("/test"))
+    assert get_default_whisper_model_parameters() == {
+        "model_size_or_path": "large-v3",
+        "download_root": "/test/.whisper",
+        "device": "cuda",
+        "compute_type": "float32",
+    }
