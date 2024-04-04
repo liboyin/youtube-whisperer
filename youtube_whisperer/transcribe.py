f84dee13c6f1763513eb9c4867483ecd720bffeb
Libo Yin <liboyin830@gmail.com>
2024-04-04 13:37:27 +0000

    Further abstracted the function to transcribe a waveform

--- youtube_whisperer/transcribe.py
+++ youtube_whisperer/transcribe.py
@@ -1,10 +1,12 @@
 import multiprocessing
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterable, Generator
 
 import ctranslate2
 from faster_whisper import WhisperModel
+from faster_whisper.transcribe import Segment
+from faster_whisper.utils import format_timestamp
 import numpy as np
 
 from utils import strtobool
@@ -49,23 +51,48 @@ def get_default_whisper_model_parameters() -> dict[str, Any]:
     return result
 
 
-def transcribe(model: WhisperModel, waveform: np.ndarray, language: str) -> dict[str, Any]:
+def yield_segments(generator: Iterable[Segment]) -> Generator[Segment, None, None]:
+    """
+    Yield segments from the given transcription generator.
+    """
+    for segment in generator:
+        print(f'{segment.id} {format_timestamp(segment.start)} -> {format_timestamp(segment.end)}: {segment.text}')
+        yield segment
+
+
+def get_transcription_generator(model: WhisperModel, waveform: np.ndarray, language: str) -> Iterable[Segment]:
+    """
+    Generates transcriptions for the given waveform using the specified Whisper model.
+
+    Args:
+        model (WhisperModel): The Whisper model used for transcription.
+        waveform (np.ndarray): The waveform to transcribe.
+        language (str): The language of the transcription.
+
+    Returns:
+        Iterable[Segment]: A generator that yields Segments of the transcription.
+    """
+    generator, info = model.transcribe(waveform, beam_size=5, task='transcribe', language=language)
+    print(info)
+    return generator
+
+
+def transcribe(model: WhisperModel, waveform: np.ndarray, language: str) -> list[Segment]:
     """
     Transcribe the given waveform using the provided WhisperModel.
 
     Args:
-        model (WhisperModel): The model to use for transcription.
+        model (WhisperModel): The Whisper model to use for transcription.
         waveform (np.ndarray): The waveform to transcribe.
         language (str): The language of the waveform.
 
     Returns:
-        dict: A dictionary containing the transcribed segments.
+        list[Segment]: Segments of the transcription.
     """
-    segment_generator, _ = model.transcribe(waveform, beam_size=5, task='transcribe', language=language)
-    return {"segments": list(segment_generator)}
+    return list(yield_segments(get_transcription_generator(model, waveform, language)))
 
 
-def transcribe_with_default_model(waveform: np.ndarray, language: str) -> dict[str, Any]:
+def transcribe_with_default_model(waveform: np.ndarray, language: str) -> list[Segment]:
     """
     Transcribe the given waveform using the default WhisperModel.
 
@@ -74,7 +101,7 @@ def transcribe_with_default_model(waveform: np.ndarray, language: str) -> dict[s
         language (str): The language of the waveform.
 
     Returns:
-        dict: A dictionary containing the transcribed segments.
+        list[Segment]: Segments of the transcription.
     """
     model = WhisperModel(**get_default_whisper_model_parameters())
     return transcribe(model, waveform, language)
