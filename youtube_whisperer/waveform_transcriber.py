9f3ec751731d54a7f4c11c7de6eaefab45a27dd8
Libo Yin <liboyin830@gmail.com>
2024-04-13 15:11:31 +0000

    Removed explicit specification of language

--- youtube_whisperer/waveform_transcriber.py
+++ youtube_whisperer/waveform_transcriber.py
@@ -9,51 +9,48 @@ from youtube_whisperer.segment_to_srt_adaptor import yield_srt_blocks_from_segme
 from youtube_whisperer.srt_deduplicator import SrtBlock, deduplicate_srt_blocks
 
 
-def get_transcription_generator(model: WhisperModel, waveform: np.ndarray, language: str, **kwargs) -> Iterable[Segment]:
+def get_transcription_generator(model: WhisperModel, waveform: np.ndarray, **kwargs) -> Iterable[Segment]:
     """
     Return a transcription generator for the given waveform using the specified Whisper model.
 
     Args:
         model (WhisperModel): The Whisper model used for transcription.
         waveform (np.ndarray): The waveform to transcribe.
-        language (str): The language of the transcription.
         kwargs: Additional arguments to pass to model.transcribe().
 
     Returns:
         Iterable[Segment]: An iterable of Segments of the transcription.
     """
-    segments, info = model.transcribe(waveform, language=language, **kwargs)
+    segments, info = model.transcribe(waveform, **kwargs)
     print(info)
     return segments
 
 
-def transcribe_waveform(model: WhisperModel, waveform: np.ndarray, language: str, **kwargs) -> list[SrtBlock]:
+def transcribe_waveform(model: WhisperModel, waveform: np.ndarray, **kwargs) -> list[SrtBlock]:
     """
     Transcribe the given waveform using the provided WhisperModel.
 
     Args:
         model (WhisperModel): The Whisper model to use for transcription.
         waveform (np.ndarray): The waveform to transcribe.
-        language (str): The language of the transcription.
         kwargs: Additional arguments to pass to model.transcribe().
 
     Returns:
         list[SrtBlock]: Transcription of the waveform as a list of SRT blocks.
     """
-    return deduplicate_srt_blocks(yield_srt_blocks_from_segments(get_transcription_generator(model, waveform, language, **kwargs)))
+    return deduplicate_srt_blocks(yield_srt_blocks_from_segments(get_transcription_generator(model, waveform, **kwargs)))
 
 
-def transcribe_waveform_with_default_model(waveform: np.ndarray, language: str, **kwargs) -> list[SrtBlock]:
+def transcribe_waveform_with_default_model(waveform: np.ndarray, **kwargs) -> list[SrtBlock]:
     """
     Transcribe the given waveform using the default WhisperModel.
 
     Args:
         waveform (np.ndarray): The waveform to transcribe.
-        language (str): The language of the transcription.
         kwargs: Additional arguments to pass to model.transcribe().
 
     Returns:
         list[SrtBlock]: Transcription of the waveform as a list of SRT blocks.
     """
     model = WhisperModel(**get_default_whisper_model_parameters())
-    return transcribe_waveform(model, waveform, language, **kwargs)
+    return transcribe_waveform(model, waveform, **kwargs)
