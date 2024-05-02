fc3030648b5d7ed60c88d02cdd121b2802a5b19b
Libo Yin <liboyin830@gmail.com>
2024-05-02 10:07:28 +0000

    Made the CLI entry point accept multiple input sources

--- youtube_whisperer/__main__.py
+++ youtube_whisperer/__main__.py
@@ -15,14 +15,14 @@ def main() -> None:
     CLI entry point to transcribe a waveform file and save results to a Segment file and an SRT file.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("source", help="Waveform file path to transcribe, or video URL to download and transcribe.")
-    source = parser.parse_args().source
-    input_file_path = download_video_with_default_title(source) if is_url(source) else Path(source)
-    segments_generator = transcribe_waveform_with_default_model(load_waveform_from_file(input_file_path))
-    segments_generator = duplicate_segments_to_file(segments_generator, input_file_path.with_suffix('.txt'))
-    srt_blocks_generator = yield_deduplicated_srt_blocks(yield_srt_blocks_from_segments(segments_generator))
-    output_file_path = prepare_output_file(input_file_path.with_suffix('.srt'))
-    output_file_path.write_text(convert_srt_blocks_to_str(srt_blocks_generator))
+    parser.add_argument("sources", nargs='+', help="Waveform file paths to transcribe, or video URLs to download and transcribe.")
+    for source in parser.parse_args().sources:
+        input_file_path = download_video_with_default_title(source) if is_url(source) else Path(source)
+        segments_generator = transcribe_waveform_with_default_model(load_waveform_from_file(input_file_path))
+        segments_generator = duplicate_segments_to_file(segments_generator, input_file_path.with_suffix('.txt'))
+        srt_blocks_generator = yield_deduplicated_srt_blocks(yield_srt_blocks_from_segments(segments_generator))
+        output_file_path = prepare_output_file(input_file_path.with_suffix('.srt'))
+        output_file_path.write_text(convert_srt_blocks_to_str(srt_blocks_generator))
 
 
 if __name__ == "__main__":
