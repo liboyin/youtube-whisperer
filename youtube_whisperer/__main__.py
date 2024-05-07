d4f6e61cf367f81334720c7a9e5c55b252092f12
Libo Yin <liboyin830@gmail.com>
2024-05-07 11:12:52 +0000

    Bypassed Whisper if a video URL already has a transcript

--- youtube_whisperer/__main__.py
+++ youtube_whisperer/__main__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from youtube_whisperer.pathlib_extensions import prepare_output_file
 from youtube_whisperer.segment_to_srt_adaptor import yield_deduplicated_srt_blocks, yield_srt_blocks_from_segments
 from youtube_whisperer.srt_deduplicator import convert_srt_blocks_to_str
+from youtube_whisperer.transcript_downloader import download_transcript_as_srt_file
 from youtube_whisperer.utils import is_url
 from youtube_whisperer.video_downloader import download_video_with_default_title
 from youtube_whisperer.waveform_loader import load_waveform_from_file
@@ -17,7 +18,12 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("sources", nargs='+', metavar='N', help="Waveform file paths to transcribe, or video URLs to download and transcribe.")
     for source in parser.parse_args().sources:
-        input_file_path = download_video_with_default_title(source) if is_url(source) else Path(source)
+        if is_url(source):
+            input_file_path = download_video_with_default_title(source)
+            if download_transcript_as_srt_file(source, input_file_path.with_suffix('.srt')):
+                continue
+        else:
+            input_file_path = Path(source)
         segments_generator = transcribe_waveform_with_default_model(load_waveform_from_file(input_file_path))
         segments_generator = duplicate_segments_to_file(segments_generator, input_file_path.with_suffix('.seg'))
         srt_blocks_generator = yield_deduplicated_srt_blocks(yield_srt_blocks_from_segments(segments_generator))
