fc46d9b9f33daa241ccebc7c68ebfa9ea37b81d9
Libo Yin <liboyin830@gmail.com>
2024-05-08 21:08:43 +1000

    Renamed all env vars to start with WHISPER_

--- tests/test_model_parameters.py
+++ tests/test_model_parameters.py
@@ -6,29 +6,29 @@ from youtube_whisperer.model_parameters import get_default_cuda_flag, get_defaul
 
 @mock.patch('ctranslate2.get_cuda_device_count')
 def test_get_default_cuda_flag_with_env_var(mock_get_cuda_device_count, monkeypatch):
-    monkeypatch.setenv("ASR_USE_CUDA", "true")
+    monkeypatch.setenv("WHISPER_USE_CUDA", "true")
     assert get_default_cuda_flag() is True
     mock_get_cuda_device_count.assert_not_called()
 
 
 @mock.patch('ctranslate2.get_cuda_device_count', return_value=0)
 def test_get_default_cuda_flag_without_env_var_cpu(mock_get_cuda_device_count, monkeypatch):
-    monkeypatch.delenv("ASR_USE_CUDA", raising=False)
+    monkeypatch.delenv("WHISPER_USE_CUDA", raising=False)
     assert get_default_cuda_flag() is False
     mock_get_cuda_device_count.assert_called_once()
 
 
 @mock.patch('ctranslate2.get_cuda_device_count', return_value=1)
 def test_get_default_cuda_flag_without_env_var_cuda(mock_get_cuda_device_count, monkeypatch):
-    monkeypatch.delenv("ASR_USE_CUDA", raising=False)
+    monkeypatch.delenv("WHISPER_USE_CUDA", raising=False)
     assert get_default_cuda_flag() is True
     mock_get_cuda_device_count.assert_called_once()
 
 
 def test_get_default_whisper_model_parameters_with_env_var_cpu(monkeypatch, mocker):
-    monkeypatch.setenv("ASR_MODEL", "small")
-    monkeypatch.setenv("ASR_MODEL_PATH", "test/path")
-    monkeypatch.setenv("ASR_USE_CUDA", "false")
+    monkeypatch.setenv("WHISPER_MODEL", "small")
+    monkeypatch.setenv("WHISPER_MODEL_PATH", "test/path")
+    monkeypatch.setenv("WHISPER_USE_CUDA", "false")
     mocker.patch('multiprocessing.cpu_count', return_value=8)
     assert get_default_whisper_model_parameters() == {
         "model_size_or_path": "small",
@@ -40,9 +40,9 @@ def test_get_default_whisper_model_parameters_with_env_var_cpu(monkeypatch, mock
 
 
 def test_get_default_whisper_model_parameters_with_env_var_cuda(monkeypatch):
-    monkeypatch.setenv("ASR_MODEL", "small")
-    monkeypatch.setenv("ASR_MODEL_PATH", "test/path")
-    monkeypatch.setenv("ASR_USE_CUDA", "true")
+    monkeypatch.setenv("WHISPER_MODEL", "small")
+    monkeypatch.setenv("WHISPER_MODEL_PATH", "test/path")
+    monkeypatch.setenv("WHISPER_USE_CUDA", "true")
     assert get_default_whisper_model_parameters() == {
         "model_size_or_path": "small",
         "download_root": "test/path",
@@ -52,7 +52,7 @@ def test_get_default_whisper_model_parameters_with_env_var_cuda(monkeypatch):
 
 
 def test_get_default_whisper_model_parameters_without_env_var_cpu(monkeypatch, mocker):
-    monkeypatch.setenv('ASR_MODEL_PATH', "/test/.whisper")
+    monkeypatch.setenv('WHISPER_MODEL_PATH', "/test/.whisper")
     mocker.patch('youtube_whisperer.model_parameters.get_default_cuda_flag', return_value=False)
     mocker.patch('multiprocessing.cpu_count', return_value=8)
     assert get_default_whisper_model_parameters() == {
@@ -65,8 +65,8 @@ def test_get_default_whisper_model_parameters_without_env_var_cpu(monkeypatch, m
 
 
 def test_get_default_whisper_model_parameters_without_env_var_cuda(monkeypatch, mocker):
-    monkeypatch.setenv('ASR_MODEL', "test_model")
-    monkeypatch.setenv('ASR_MODEL_PATH', "/test/.whisper")
+    monkeypatch.setenv('WHISPER_MODEL', "test_model")
+    monkeypatch.setenv('WHISPER_MODEL_PATH', "/test/.whisper")
     mocker.patch('youtube_whisperer.model_parameters.get_default_cuda_flag', return_value=True)
     assert get_default_whisper_model_parameters() == {
         "model_size_or_path": "test_model",
--- youtube_whisperer/model_parameters.py
+++ youtube_whisperer/model_parameters.py
@@ -10,14 +10,14 @@ from youtube_whisperer.utils import DEFAULT_HOME_DIR, strtobool
 
 def get_default_cuda_flag() -> bool:
     """
-    This function checks the environment variable ASR_USE_CUDA to determine whether to use CUDA for GPU acceleration.
+    This function checks the environment variable WHISPER_USE_CUDA to determine whether to use CUDA for GPU acceleration.
     If the environment variable is not set, it checks if there is a CUDA device available on the system.
     If a CUDA device is available, it returns True; otherwise, it returns False.
 
     Returns:
         bool: True if CUDA should be used, False otherwise.
     """
-    gpu = os.getenv("ASR_USE_CUDA", None)
+    gpu = os.getenv("WHISPER_USE_CUDA", None)
     if gpu is None:
         return ctranslate2.get_cuda_device_count() > 0
     return strtobool(gpu)
@@ -30,8 +30,8 @@ def get_default_whisper_model_parameters() -> dict[str, Any]:
     The parameters are determined based on the environment variables and the available hardware.
     """
     result: dict[str, Any] = {
-        "model_size_or_path": os.getenv("ASR_MODEL", "large-v3"),
-        "download_root": os.getenv("ASR_MODEL_PATH", str(DEFAULT_HOME_DIR)),
+        "model_size_or_path": os.getenv("WHISPER_MODEL", "large-v3"),
+        "download_root": os.getenv("WHISPER_MODEL_PATH", str(DEFAULT_HOME_DIR)),
     }
     use_cuda = get_default_cuda_flag()
     result["device"] = "cuda" if use_cuda else "cpu"
