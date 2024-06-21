82c965a99392e23d3270b80ec18b36188fb01f41
Libo Yin <liboyin830@gmail.com>
2024-06-21 13:56:32 +0000

    Fixed an issue where this project may be included in requirements.txt

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-pip3 freeze | grep -v youtube_whisperer > requirements.txt
+pip3 freeze | grep -v whisperer > requirements.txt
 # deployment dependencies are installed as user, but current user is vscode
 sudo pip3 --disable-pip-version-check --no-cache-dir install --editable .[dev]
-pip3 freeze | grep -v youtube_whisperer > requirements.dev.txt
+pip3 freeze | grep -v whisperer > requirements.dev.txt
