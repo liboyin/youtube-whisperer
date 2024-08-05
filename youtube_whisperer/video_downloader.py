9de09eb5daded96604c2912e0539bf7343563f71
Libo Yin <liboyin830@gmail.com>
2024-08-05 04:17:50 +0000

    Added a feature to access YouTube with Firefox cookies

--- youtube_whisperer/video_downloader.py
+++ youtube_whisperer/video_downloader.py
@@ -25,6 +25,23 @@ def get_video_title(url: str) -> str:
         return ydl.extract_info(url, download=False)['title']
 
 
+def is_firefox_cookies_file_available() -> bool:
+    """
+    Return whether the Firefox cookies file is available in the file system.
+    """
+    for search_dir_path in [
+        Path('~/.mozilla/firefox'),
+        Path('~/snap/firefox/common/.mozilla/firefox'),
+        Path('~/.var/app/org.mozilla.firefox/.mozilla/firefox'),
+    ]:
+        for cookies_file_path in search_dir_path.rglob('cookies.sqlite'):
+            if cookies_file_path.is_file():
+                print('Found Firefox cookies file:', cookies_file_path)
+                return True
+    print('No Firefox cookies file found')
+    return False
+
+
 def download_video(url: str, target_path: Path, overwrite: bool = False) -> None:
     """
     Downloads a video from a given URL and saves it to the target path.
@@ -43,6 +60,8 @@ def download_video(url: str, target_path: Path, overwrite: bool = False) -> None
         'outtmpl': str(target_path),
         'verbose': True,
     }
+    if is_firefox_cookies_file_available():
+        ydl_opts['cookiesfrombrowser'] = ('firefox',)
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
