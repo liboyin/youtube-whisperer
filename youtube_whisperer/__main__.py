0ced778aa307fad8b37d18d32a3214a9fafe2975
Libo Yin <liboyin830@gmail.com>
2024-09-20 15:13:36 +0000

    Non-functional refactoring on the project entrypoint

--- youtube_whisperer/__main__.py
+++ youtube_whisperer/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from typing import Iterable
 
 from youtube_whisperer.playlist_downloader import yield_video_urls_from_playlist
 from youtube_whisperer.segment_to_srt_adaptor import convert_segments_file_to_srt
@@ -9,18 +10,18 @@ from youtube_whisperer.video_downloader import download_video_with_default_title
 from youtube_whisperer.waveform_transcriber import transcribe_file_with_default_model
 
 
-def get_video_urls_and_file_paths(sources: list[str]) -> tuple[list[str], list[Path]]:
+def get_urls_and_file_paths(sources: list[str]) -> tuple[list[str], list[Path]]:
     """
-    Extract video URLs and file paths from a list of sources.
+    Extract video URLs and waveform file paths from a list of sources.
 
     Args:
-        sources (list[str]): A list containing video URLs, playlist URLs, or paths to video files.
+        sources (list[str]): A list containing video URLs, playlist URLs, or paths to waveform files.
 
     Returns:
-        tuple[list[str], list[Path]]: A tuple containing a list of video URLs and a list of video file paths.
+        tuple[list[str], list[Path]]: A tuple containing a list of video URLs and a list of waveform file paths.
     """
-    video_urls = []
-    video_file_paths = []
+    video_urls: list[str] = []
+    file_paths: list[Path] = []
     for source in sources:
         if is_url(source):
             if 'playlist' in source:
@@ -28,8 +29,58 @@ def get_video_urls_and_file_paths(sources: list[str]) -> tuple[list[str], list[P
             else:
                 video_urls.append(source)
         else:
-            video_file_paths.append(Path(source))
-    return video_urls, video_file_paths
+            file_paths.append(Path(source))
+    return video_urls, file_paths
+
+
+def download_video_and_transcript(url: str, lang_codes: Iterable[str] | None) -> tuple[Path, bool]:
+    """
+    Downloads a video and its transcript from a given URL.
+
+    Args:
+        url (str): The URL of the video to download.
+        lang_codes (Iterable[str] | None): Language codes to filter available transcripts with. If None, use `DEFAULT_LANG_CODES`.
+
+    Returns:
+        tuple[Path, bool]: A tuple containing the path to the downloaded video file and a boolean indicating whether the transcript was successfully downloaded.
+    """
+    video_file_path = download_video_with_default_title(url)
+    transcript_flag = download_transcript_as_srt_file(url, video_file_path.with_suffix('.srt'), lang_codes)
+    return video_file_path, transcript_flag
+
+
+def try_download_videos_and_transcripts(urls: Iterable[str], lang_codes: Iterable[str] | None) -> list[Path]:
+    """
+    Attempts to download videos and their transcripts from a list of URLs.
+
+    Args:
+        urls (Iterable[str]): A collection of video URLs to download.
+        lang_codes (Iterable[str] | None): Language codes to filter available transcripts with. If None, use `DEFAULT_LANG_CODES`.
+
+    Returns:
+        list[Path]: A list of Paths to video files that were downloaded but did not have transcripts.
+    """
+    video_files_without_transcripts: list[Path] = []
+    for url in urls:
+        video_file_path, transcript_flag = download_video_and_transcript(url, lang_codes)
+        if not transcript_flag:
+            video_files_without_transcripts.append(video_file_path)
+    return video_files_without_transcripts
+
+
+def transcribe_to_srt_files(input_file_paths: Iterable[Path], language: str | None) -> None:
+    """
+    Transcribes a list of waveform files using the default model and converts the transcriptions to SRT format.
+
+    Args:
+        input_file_paths (Iterable[Path]): An iterable of file paths to the waveform files to be transcribed.
+        language (str | None): The language code for the transcription. If None, the language will be automatically detected.
+    """
+    for input_file_path in input_file_paths:
+        segment_file_path = transcribe_file_with_default_model(input_file_path, language=language)
+        print('Saved Segments file:', segment_file_path)
+        srt_file_path = convert_segments_file_to_srt(segment_file_path, deduplicate=True)
+        print('Saved SRT file:', srt_file_path)
 
 
 def main() -> None:
@@ -37,23 +88,13 @@ def main() -> None:
     CLI entry point to transcribe waveform files and save each result to a Segment file and an SRT file.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("sources", nargs='+', metavar='source', help="Waveform file paths to transcribe, or video URLs to download and transcribe.")
+    parser.add_argument("sources", nargs='+', metavar='source', help="Waveform file paths to transcribe, or video/playlist URLs to download and transcribe.")
     parser.add_argument("-l", "--language", type=str, default=None, help="Language for transcription")
     args = parser.parse_args()
     language = get_verified_language(args.language)
-    lang_codes = [language] if language else None
-    video_urls, video_file_paths = get_video_urls_and_file_paths(args.sources)
-    for video_url in video_urls:
-        # downloaders do not overwrite by default because they are almost transactional
-        video_file_path = download_video_with_default_title(video_url)
-        if not download_transcript_as_srt_file(video_url, video_file_path.with_suffix('.srt'), lang_codes):
-            video_file_paths.append(video_file_path)
-    for video_file_path in video_file_paths:
-        # local operations overwrite by default because they may fail half way through
-        segment_file_path = transcribe_file_with_default_model(video_file_path, language=language)
-        print('Saved Segments file:', segment_file_path)
-        srt_file_path = convert_segments_file_to_srt(segment_file_path, deduplicate=True)
-        print('Saved SRT file:', srt_file_path)
+    video_urls, file_paths = get_urls_and_file_paths(args.sources)
+    file_paths.extend(try_download_videos_and_transcripts(video_urls, [language] if language else None))
+    transcribe_to_srt_files(file_paths, language)
 
 
 if __name__ == "__main__":
