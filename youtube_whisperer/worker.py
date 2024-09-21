062579ff3bf0f97387b84ca70a2d55ffcf70f9eb
Libo Yin <liboyin830@gmail.com>
2024-09-22 01:11:46 +1000

    Fixed several issues in the Redis queue monitor

--- youtube_whisperer/worker.py
+++ youtube_whisperer/worker.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import time
 
 from youtube_whisperer.__main__ import transcribe_to_srt_files
@@ -7,15 +8,24 @@ from youtube_whisperer.utils import redis_connection, is_url
 
 
 def process_queue():
+    """
+    Continuously monitors and processes download and transcription tasks from a Redis queue.
+    """
     with redis_connection() as client:
+        print("Worker started")
         while True:
             task = client.lindex('tasks', 0)
             if task:
-                source, language = json.loads(task)
-                print(f"Processing task: {source}, {language}")
+                print(f"Processing task: {task}")
+                task = json.loads(task)
+                source = task['source']
+                language = task['language']
                 # assume playlists and glob patterns have been resolved at insertion time
                 if is_url(source):
                     video_file_path, transcript_flag = download_video_and_transcript_with_default_title(source, language)
+                else:
+                    video_file_path = Path(source)
+                    transcript_flag = False
                 if not transcript_flag:
                     transcribe_to_srt_files([video_file_path], language)
                 client.lpop('tasks')
