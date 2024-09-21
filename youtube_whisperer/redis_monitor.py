5a4b3de5b720dbe2a3ad3b636e43fd261daca456
Libo Yin <liboyin830@gmail.com>
2024-09-21 10:35:27 +0000

    Added a monitor for the Redis queue

--- youtube_whisperer/redis_monitor.py
+++ youtube_whisperer/redis_monitor.py
@@ -1,7 +1,9 @@
 import json
 import time
 
-from youtube_whisperer.utils import redis_connection
+from youtube_whisperer.__main__ import transcribe_to_srt_files
+from youtube_whisperer.downloaders import download_video_and_transcript_with_default_title
+from youtube_whisperer.utils import redis_connection, is_url
 
 
 def process_queue():
@@ -11,6 +13,11 @@ def process_queue():
             if task:
                 source, language = json.loads(task)
                 print(f"Processing task: {source}, {language}")
+                # assume playlists and glob patterns have been resolved at insertion time
+                if is_url(source):
+                    video_file_path, transcript_flag = download_video_and_transcript_with_default_title(source, language)
+                if not transcript_flag:
+                    transcribe_to_srt_files([video_file_path], language)
                 client.lpop('tasks')
             else:
                 print("Queue is empty")
