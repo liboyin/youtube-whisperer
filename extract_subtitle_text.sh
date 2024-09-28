06a751951e4e89cc9d4ac1715cac168734fc7a15
Libo Yin <liboyin830@gmail.com>
2024-09-28 19:42:58 +1000

    Fixed an issue where a wrong bash script was removed

---
+++ extract_subtitle_text.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if a file name is provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <srt_file>"
+    exit 1
+fi
+
+# Use sed to process the SRT file
+# -e '/-->/d' will delete the time range lines
+# -e '/^[0-9]*$/d' will delete lines that only contain numbers (sequence numbers)
+# -e '/^$/d' will delete empty lines
+sed -e '/-->/d' -e '/^[0-9]*$/d' -e '/^$/d' "$1"
+
+# To run this script over all .srt files:
+# find . -name "*.srt" -exec sh -c './extract_subtitle_text.sh "$1" > "${1%.srt}.txt"' _ {} \;
