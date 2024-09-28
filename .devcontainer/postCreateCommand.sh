09f57b6b81d997680b37222ddafba9c1bf962b6f
Libo Yin <liboyin830@gmail.com>
2024-09-28 19:34:42 +1000

    Cleaned up the devcontainer post-build script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-pip3 freeze | grep -v whisperer > requirements.txt
+pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as user, but current user is vscode
-sudo pip3 --disable-pip-version-check --no-cache-dir install --editable .[dev]
-pip3 freeze | grep -v whisperer > requirements.dev.txt
+sudo pip --disable-pip-version-check --no-cache-dir install --editable .[dev]
+pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.dev.txt
