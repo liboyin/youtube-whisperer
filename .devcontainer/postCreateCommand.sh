2da60e55aa1a0c7bd228ac7a65007c8a7c7ee8e5
Libo Yin <liboyin830@gmail.com>
2024-10-26 00:38:21 +1100

    Removed requirements.dev.txt

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,3 @@
 pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as user, but current user is vscode
 sudo pip --disable-pip-version-check --no-cache-dir install --editable .[dev]
-pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.dev.txt
