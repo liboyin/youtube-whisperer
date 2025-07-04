03b849e5c5faced973f94422164ec2396d72cb2f
Libo Yin <liboyin830@gmail.com>
2025-07-05 01:14:25 +1000

    Modified postCreateCommand.sh to support Claude Code

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,8 @@
 pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as root, but current user is vscode
 sudo pip install -e .[dev]
-# install Gemini CLI
+# install nodeJS for AI code assistants
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends nodejs npm
-sudo npm install -g @google/gemini-cli
+# npx @google/gemini-cli
+# npx @anthropic-ai/claude-code
