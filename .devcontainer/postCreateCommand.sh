bcdb173dcb523309fece0fdc0cfe21069a101104
Libo Yin <liboyin830@gmail.com>
2025-07-10 11:41:29 +0000

    Fixed a documentation inconsistency

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
-# deployment dependencies are installed as root, but current user is vscode
+# deployment dependencies are installed as root, but current user is ubuntu
 sudo pip install -e .[dev]
 # install nodeJS for AI code assistants
 sudo apt-get update
