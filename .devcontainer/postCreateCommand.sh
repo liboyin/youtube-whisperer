1584d7ef175c2d92c73ee1a60389a6c3e5a4572b
Libo Yin <liboyin830@gmail.com>
2025-08-18 13:48:05 +0000

    Updated NodeJS to v20

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,6 +4,7 @@ pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 sudo pip install -e .[dev]
 # install nodeJS for AI code assistants
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends nodejs npm
-# npx @google/gemini-cli
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y --no-install-recommends nodejs
+# npx https://github.com/google-gemini/gemini-cli
 # npx @anthropic-ai/claude-code
