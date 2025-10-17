14deb69ab05c041c6ea02820fe6ece273ec6454e
Libo Yin <liboyin830@gmail.com>
2025-10-17 11:22:35 +0000

    Rewrote installation logic for Claude Code and Gemini CLI in the devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,10 @@
 pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as root, but current user is ubuntu
 sudo pip install -e .[dev]
-# install nodeJS for AI code assistants
+# use native installer of Claude Code
+curl -fsSL https://claude.ai/install.sh | bash
+# install nodeJS + Gemini CLI
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y --no-install-recommends nodejs
-# npx https://github.com/google-gemini/gemini-cli
-# npx @anthropic-ai/claude-code
+sudo npm install -g @google/gemini-cli
