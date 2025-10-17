1256c1730208757a6514e7a6626b57b557a71a26
Libo Yin <liboyin830@gmail.com>
2025-10-17 11:49:23 +0000

    Updated NodeJS from 20 to 25

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,6 +6,6 @@ sudo pip install -e .[dev]
 curl -fsSL https://claude.ai/install.sh | bash
 # install nodeJS + Gemini CLI
 sudo apt-get update
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_25.x | sudo -E bash -
 sudo apt-get install -y --no-install-recommends nodejs
 sudo npm install -g @google/gemini-cli
