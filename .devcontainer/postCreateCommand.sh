d80afe244b7665412728d2a3dd975a2a30ba988d
Libo Yin <liboyin830@gmail.com>
2026-03-20 00:18:39 +1100

    Updated vscode plugins and AI dev tools

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,9 @@ pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as root, but current user is ubuntu
 sudo pip install -e .[dev]
 # use native installer of Claude Code
-curl -fsSL https://claude.ai/install.sh | bash
+# curl -fsSL https://claude.ai/install.sh | bash
 # install nodeJS + Gemini CLI
-sudo npm install -g @google/gemini-cli
+# sudo apt-get update
+# curl -fsSL https://deb.nodesource.com/setup_25.x | sudo -E bash -
+# sudo apt-get install -y --no-install-recommends nodejs
+# sudo npm install -g @google/gemini-cli
