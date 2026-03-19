f9c094d78568ad0fdba9fd05641498aeeb20436c
Libo Yin <liboyin830@gmail.com>
2026-03-20 01:20:09 +1100

    Add set -euo pipefail in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -euo pipefail
 pip freeze | grep -v "youtube-whisperer\|youtube_whisperer" > requirements.txt
 # deployment dependencies are installed as root, but current user is ubuntu
 sudo pip install -e .[dev]
